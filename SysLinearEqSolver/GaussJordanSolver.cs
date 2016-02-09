4379b48461165ffc48c3657c4fa1a9c4453b0c9e
vasya <vasya@gmail.com>
2016-02-09 23:08:08 +0200

    upd_gaus_jord

--- SysLinearEqSolver/GaussJordanSolver.cs
+++ SysLinearEqSolver/GaussJordanSolver.cs
@@ -9,9 +9,51 @@ namespace SysLinearEqSolver
     {
         public override void Solve(double[][] A, double[] B)
         {
-            throw new NotImplementedException();
+            int N = A.GetLength(0);
+            // throw new NotImplementedException();
+            double[] x = new double[N];
 
-            //this.Results = results array
+            //Console.WriteLine("row A");
+            //Console.WriteLine(string.Join("\n", A.Select(a => string.Concat(a.Select(v => string.Format("{0,12:F8}", v))))));
+            //Console.WriteLine("vector B");
+            //Console.WriteLine(string.Join("\n", B.Select(v => string.Format("{0,12:F8}", v))));
+            //Console.WriteLine();
+
+            //start
+            for (int i = 0; i < N - 1; i++)
+            {
+                for (int j = i + 1; j < N; j++)
+                {
+                    double s = A[j][i] / A[i][i];
+                    for (int k = i; k < N; k++)
+                    {
+                        A[j][k] -= A[i][k] * s;
+                    }
+                    B[j] -= B[i] * s;
+                }
+            }
+
+            //retreat 
+            for (int i = N - 1; i >= 0; i--)
+            {
+                B[i] /= A[i][i];
+                A[i][i] /= A[i][i];
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    double s = A[j][i] / A[i][i];
+                    A[j][i] -= s;
+                    B[j] -= B[i] * s;
+                }
+            }
+
+            /*
+            for (int i = 0; i < N; i++)
+            {
+                x[i] = B[i] / A[i][i];
+            }
+            */
+            x = Enumerable.Range(0, N).Select(i => B[i] / A[i][i]).ToArray();
+            this.Results = x;
         }
     }
 }
--- SysLinearEqSolver/Program.cs
+++ SysLinearEqSolver/Program.cs
@@ -10,19 +10,81 @@ namespace SysLinearEqSolver
     {
         static void Main(string[] args)
         {
-            double[,] A = { {2,1},
-                             {5,7} };
-            double[] B = { 11, 13 };
+            //double[,] A = { {2,1},
+            //                 {5,7} };
+            Console.Write("Enter number of equetions: ");
+            int n = int.Parse(Console.ReadLine());
+            int m = n;
+            // Console.Write("Введите m: ");
+            // int m = int.Parse(Console.ReadLine());
+            Console.WriteLine("\n");
+            Console.WriteLine("Filling matrix:");
+            double[,] A = new double[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.WriteLine("[{0},{1}]=", i, j);
 
-            double[] initialGuess = { 1, 1 };
+                    A[i, j] = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("\n");
+                }
+            }
+            Console.WriteLine("\n");
+            Console.Write("Enter B: ");
+            //double[] B = { 11, 13 };
+
+            double[] B = new double[n];
+
+            for (int j = 0; j < m; j++)
+            {
+                Console.WriteLine("[{0}]=", j);
+
+                B[j] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n");
+            }
+
+
+            // double[] initialGuess = { 1, 1 };
+            Console.WriteLine("\n");
+            Console.Write("Enter initial guess: ");
+            double[] initialGuess = new double[n];
+
+            for (int j = 0; j < m; j++)
+            {
+                Console.WriteLine("[{0}]=", j);
+
+                initialGuess[j] = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("\n");
+            }
+
+            //EquationsSystem system = new SqLinearEquationsSystem(A, B);
+            //JacobiMethodSolver solver = new JacobiMethodSolver(initialGuess);
+
+            //system.Solver = solver;
+            //system.Solve();
+
+            //Console.WriteLine(system.PrintResults());
+
+
+            Console.WriteLine("Jacobi method:\n");
 
             EquationsSystem system = new SqLinearEquationsSystem(A, B);
-            JacobiMethodSolver solver = new JacobiMethodSolver(initialGuess);
+            JacobiMethodSolver solver = new JacobiMethodSolver();
 
             system.Solver = solver;
             system.Solve();
 
             Console.WriteLine(system.PrintResults());
+
+            Console.WriteLine("Gauss-Jordan method:\n");
+            EquationsSystem system1 = new SqLinearEquationsSystem(A, B);
+            GaussJordanSolver solver1 = new GaussJordanSolver();
+
+            system1.Solver = solver1;
+            system1.Solve();
+
+            Console.WriteLine(system1.PrintResults());
         }
     }
 }
